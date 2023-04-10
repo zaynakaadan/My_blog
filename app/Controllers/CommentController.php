ef48e4c416d4e3887c1259f3d32dd8be2e41eed3
ZAYNA <zk10-91@hotmail.com>
2023-04-10 14:45:02 +0200

    add a security measure

--- app/Controllers/CommentController.php
+++ app/Controllers/CommentController.php
@@ -8,15 +8,21 @@ use App\Controllers\Controller;
 
 class CommentController extends Controller {
 
-    public function createcomment()
+  public function createcomment()
   {
+    foreach($_POST as $k => $p)
+    {
+        //$_POST[$k] = htmlspecialchars($_POST[$k]);
+        $_POST[$k] = strip_tags($_POST[$k]);            
+    }
+        
     $comment = new Comment($this->getDB());
     $result = $comment->create($_POST);
     
     if ($result) {
    return header("Location: " . $_SERVER ['REDIRECT_URL']);
   }
-  }
+}
 
   
   
